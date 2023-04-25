ffd8e0ca5f373b623212fea5a13c9383be4e801a
Harshit Raj <harshitraj2212@gmail.com>
2023-04-25 12:55:44 +0100

    Add files via upload
    First Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let text = document.getElementById('text');
+let gateLeft = document.getElementById('gate-left');
+let gateRight = document.getElementById('gate-right');
+
+window.addEventListener('scroll', ()=> {
+    let value = window.scrollY;
+
+    text.style.marginTop = value * 2.5 + 'px';
+    gateLeft.style.left = value * -2.5 + 'px';
+    gateRight.style.left = value * 2.5 + 'px';
+
+});
+
+// Constructing cards
